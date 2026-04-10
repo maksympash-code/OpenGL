80bc5f98bf099118e040593ebc8fc7c48fe1efc5
maksympash <maksympash@gmail.com>
2026-04-10 17:57:21 +0300

    Lecture №6 is completed: updated main.cpp and added gitignore file

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,7 @@
+#include <iostream>
+#include <ostream>
+
+#include "glad/glad.h"
 #include <GLFW/glfw3.h>
 
 int main()
@@ -8,10 +12,15 @@ int main()
     if (!glfwInit())
         return -1;
 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
     if (!window)
     {
+        std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
@@ -19,11 +28,20 @@ int main()
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+
     /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
+    while (!glfwWindowShouldClose(window) && !glfwGetKey(window, GLFW_KEY_ESCAPE))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
+        /////// gl-команди для налащтування інструментів малювання та малювання
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
