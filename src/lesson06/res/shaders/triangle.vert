1489030a97607319737a8ccd9025512596ae934c
maksympash <maksympash@gmail.com>
2026-05-13 21:31:33 +0300

    lesson06: textures.

---
+++ src/lesson06/res/shaders/triangle.vert
@@ -0,0 +1,8 @@
+#version 330 core
+
+in vec4 aPos;
+in vec2 aUV;
+
+void main() {
+    gl_Position = aPos;
+}
---
+++ src/utils/texture.h
@@ -0,0 +1,13 @@
+//
+// Created by maksympash on 13.05.2026.
+//
+
+#ifndef OPENGL_TEXTURE_H
+#define OPENGL_TEXTURE_H
+#include <string>
+
+
+unsigned int loadTexture(const std::string &fileName);
+
+
+#endif //OPENGL_TEXTURE_H
